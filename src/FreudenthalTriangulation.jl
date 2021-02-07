f82156a47738571ee0b0f0682d399025dd9d1891
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2021-02-07 09:51:03 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/FreudenthalTriangulation.jl
@@ -0,0 +1,5 @@
+module FreudenthalTriangulation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FreudenthalTriangulation
+using Test
+
+@testset "FreudenthalTriangulation.jl" begin
+    # Write your tests here.
+end
