69122440d0009469de597ab4b5d1aff917182384
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2021-02-07 20:18:27 -0800

    Did stuff

--- src/FreudenthalTriangulation.jl
+++ src/FreudenthalTriangulation.jl
@@ -1,5 +1,102 @@
 module FreudenthalTriangulation
 
-# Write your package code here.
+using LinearAlgebra
+using SparseArrays
 
+export freudenthal_vertices, freudenthal_simplex, barycentric_coordinates, freudenthal_simplex_and_coords,
+        freudenthal_simplex_and_coords!
+
+
+# Construct the Freudenthal triangulation of the belief simplex
+
+"""
+    freudenthal_vertices(n::Int64, m::Int64)
+Construct the list of Freudenthal vertices in an `n` dimensional space with grid resolution `m`.
+The vertices are represented by a list of `n` dimensional vectors.
+"""
+function freudenthal_vertices(n::Int64, m::Int64)
+    V = Vector{Int}[]
+    v = Vector{Int}(undef, n)
+    v[1] = m
+    freudenthal_vertices!(V, v, 2)
+    return V
 end
+
+function freudenthal_vertices!(V::Vector{Vector{Int64}}, v::Vector{Int64}, i::Int64)
+    n = length(v)
+    if i > n
+        push!(V, copy(v))
+        return
+    end
+    for k in 0 : v[i-1]
+        v[i] = k
+        freudenthal_vertices!(V, v, i+1)
+    end
+end
+
+"""
+    freudenthal_simplex(x::Vector{Float64})
+Returns the list of vertices of the simplex of point `x` in the Freudenthal grid.
+"""
+function freudenthal_simplex(x::Vector{Float64})
+    n = length(x)
+    V = Vector{Vector{Int}}(undef, n+1)
+    V[1] = floor.(Int, x)
+    d = x - V[1]
+    p = sortperm(d, rev=true)
+    for i in 2 : n+1
+        V[i] = copy(V[i-1])
+        V[i][p[i-1]] += 1
+    end
+    return V
+end
+
+"""
+    barycentric_coordinates(x::Vector{Int64}, V::Vector{Vector{Int64}})
+Given a point `x` and its simplex `V` in the Freudenthal grid, returns the barycentric coordinates
+of `x` in the grid. `V` must be in the same order as provided by the output of `freudenthal_simplex`
+"""
+function barycentric_coordinates(x, V)
+    d = x - V[1]
+    p = sortperm(d, rev=true)
+    n = length(x)
+    λ = Vector{Float64}(undef, n+1)
+    λ[n+1] = d[p[n]]
+    for i in n:-1:2
+        λ[i] = d[p[i-1]] - d[p[i]]
+    end
+    λ[1] = 1.0 - sum(λ[2:end])
+    return λ
+end
+
+"""
+    freudenthal_coords(x::AbstractArray{Float64})
+Given a point `x`, returns the simplex of the point `x` and the barycentric coordinates of `x` in the grid.
+"""
+function freudenthal_simplex_and_coords(x::Vector{Float64})
+    V = freudenthal_simplex(x)
+    return V, barycentric_coordinates(x, V)
+end
+
+"""
+    freudenthal_simplex_and_coords!(x::AbstractArray{Float64}, V::Vector{Vector{Int64}}, λ::Vector{Float64})
+Fills `V` and `λ` with the simplex points in the Freudenthal space and associated coordinates respectively.
+"""
+function freudenthal_simplex_and_coords!(x::AbstractArray{Float64}, V::Vector{Vector{Int64}}, λ::Vector{Float64})
+    n = length(x)
+    V[1] = floor.(Int, x)
+    d = x - V[1]
+    p = sortperm(d, rev=true)
+    for i in 2 : n+1
+        copyto!(V[i], V[i-1])
+        V[i][p[i-1]] += 1
+    end
+    λ[n+1] = d[p[n]]
+    for i in n:-1:2
+        λ[i] = d[p[i-1]] - d[p[i]]
+    end
+    λ[1] = 1.0 - sum(λ[2:end])
+    return V, λ
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,57 @@
 using FreudenthalTriangulation
 using Test
 
-@testset "FreudenthalTriangulation.jl" begin
-    # Write your tests here.
+@testset "FreudenthalTriangulations.jl" begin
+    # test freudenthal_vertices
+    n, m = rand(1:10, 2)
+    V = freudenthal_vertices(n, m)
+    @test size(V, 1) == binomial(n + m - 1, m)
+    v = rand(V)
+    @test size(v, 1) == n && v[1] == m
+    @test issorted(v, rev=true)
+
+    # test freudenthal_simplex
+    n = rand(5:20)
+    x = rand(Float64, n)
+    V = freudenthal_simplex(x)
+    @test size(V, 1) == n + 1
+    x = rand(Int64, n)
+    x = convert(Vector{Float64}, x)
+    V = freudenthal_simplex(x)
+    distances = [abs(sum(x - V[i])) for i in 1:n]
+    @test issorted(distances)
+
+    # test barycentric_coordinates
+    n = rand(5:20)
+    x = rand(Float64, n)
+    V = freudenthal_simplex(x)
+    λ = barycentric_coordinates(x, V)
+    @test size(λ, 1) == n + 1
+    @test abs(sum(λ) - 1.0) < 1e-8
+
+    # test freudenthal_simplex_and_coords!
+    n = rand(5:20)
+    x = rand(Float64, n)
+    V = freudenthal_simplex(x)
+    λ = barycentric_coordinates(x, V)
+    V2, λ2 = freudenthal_simplex_and_coords(x)
+    i = rand(1:n)
+    v = V[i]
+    v2 = V2[i]
+    @test abs(sum(v - v2)) < 1e-8
+    @test abs(sum(λ - λ2)) < 1e-8
+
+    # test freudenthal_simplex_and_coords!
+    n = rand(5:20)
+    x = rand(Float64, n)
+    V = freudenthal_simplex(x)
+    λ = barycentric_coordinates(x, V)
+    V2 = [rand(Int64, n) for i=1:n+1]
+    λ2 = rand(Float64, n+1)
+    V2, λ2 = freudenthal_simplex_and_coords!(x, V2, λ2)
+    i = rand(1:n)
+    v = V[i]
+    v2 = V2[i]
+    @test abs(sum(v - v2)) < 1e-8
+    @test abs(sum(λ - λ2)) < 1e-8
 end
